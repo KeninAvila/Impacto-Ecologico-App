c8bb44a0f2321e10901b49a3b035bf970d7cf599
KeninAvila <149337855+KeninAvila@users.noreply.github.com>
2023-11-26 11:27:03 -0500

    Punto Uno

---
+++ src/main/java/com/mycompany/impacto/ecologico/app/ImpactoEcologicoApp.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.impacto.ecologico.app;
+
+/**
+ *
+ * ------------------ Punto 1: Invertigar en Páginas Web el impacto Ecológico -------------
+ * 
+ * En las Bicicletas vamos a considerar: El número de engranajes, materiales reciclados, neumáticos, frenos, peso, 
+ * año de fabricación y distancia promedio recorrido.
+ * 
+ * En los Autos vamos a considerar: El impacto de CO2, el consumo de combustible, el convertidor catalítico, 
+ * el modo híbrido, el uso de biocombustible y la capacidad de pasajeros del auto.
+ * 
+ * En los edificios vamos a considerar: El impacto por pisos, el consumo de luz, la cantidad de desechos, el consumo de agua,
+ * la cantidad de personas, el uso de energía renovable, la presencia de áreas verdes y el impacto del transporte del edificio
+ * 
+ * 
+ */
+public class ImpactoEcologicoApp {
+
+    
+}
